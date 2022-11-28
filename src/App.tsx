2559fe14c1203d9e0f1f029c0518277896a9ae1a
Maxwell Mandina MAC <maxwell.mandina@gmail.com>
2022-11-28 14:19:06 -0500

    Interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  monsters: Monster[];
+}
+interface Props {}
+interface Monster {
+  name: string;
+  id: number;
+}
+class App extends Component<Props, State> {
+  state: State = {
+    monsters: [
+      {
+        name: 'Phyllis',
+        id: 1,
+      },
+      {
+        name: 'Louis',
+        id: 2,
+      },
+      {
+        name: 'Franklin',
+        id: 3,
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.monsters.map((monster) => (
+          <h1>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
